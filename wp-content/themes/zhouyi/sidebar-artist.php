836a362e7dafdfd8900c62869190c1dc2074a911
陆秋石 <uicestone@gmail.com>
2014-04-01 01:07:40 +0800

    #15 艺术家副导航图片替换

--- wp-content/themes/zhouyi/sidebar-artist.php
+++ wp-content/themes/zhouyi/sidebar-artist.php
@@ -8,6 +8,17 @@
 		Digital Entertainment Arts Ltd
 	</div>
 	<ul class="artist-list">
-		<li><img src="<?=get_template_directory_uri()?>/images/artist_tree.jpg"></li>
+		<li>
+			<img src="<?= get_template_directory_uri() ?>/images/artist_iceberg.jpg">
+		</li>
+		<li>
+			<img src="<?= get_template_directory_uri() ?>/images/artist_gripoix.png">
+		</li>
+		<li>
+			<img src="<?= get_template_directory_uri() ?>/images/artist_yoox.com.jpg">
+		</li>
+		<li>
+			<img src="<?= get_template_directory_uri() ?>/images/artist_each-x-other.jpg">
+		</li>
 	</ul>
 </div>
