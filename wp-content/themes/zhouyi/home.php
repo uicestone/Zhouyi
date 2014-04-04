32e0ddca59a50b87c5d19c8c5d02069a26e0571d
陆秋石 <uicestone@gmail.com>
2014-04-04 10:59:10 +0800

    完成 #17, #16, #9

--- wp-content/themes/zhouyi/home.php
+++ wp-content/themes/zhouyi/home.php
@@ -2,7 +2,11 @@
 <div class="content">
 	<div class="main"></div>
 	<div class="right">
-		<div class="intro">
+		<div class="intro-language-switcher">
+			<button type="button" class="zh-cn" target-lang="zh-cn"></button>
+			<button type="button" class="en" target-lang="en"></button>
+		</div>
+		<div class="intro" lang="en">
 			<p>
 				Chinese multimedia artist who has lived in Rome from the age of ten and 
 				studied between London and Paris with degrees in Political Science and 
@@ -51,12 +55,23 @@
 				celebrities and wealthy patrons and young social media audience
 			</p>
 		</div>
+		<div class="intro" lang="zh-cn" style="display: none;">
+			<p>华人多媒体艺术家周依10岁定居罗马，之后在伦敦和巴黎求学并获得政治学和经济学学位。周依是一位通晓多国语言的艺术家,在过去的10年里她的作品在上海双年展，威尼斯双年展，圣丹斯电影节，以及戛纳电影节上展出。</p>
+			<p>从2010年到2011年，周依是有史以来第一位作为一个美容品牌（娇韵诗）的艺术家大使。周依作为多重身份艺术家和以她涉及多种领域的的特点使她受到奢侈品牌的喜爱。从2012年起，她开始和一些奢侈品牌进行合作，例如意大利太阳镜品牌Persol和Hogan。</p>
+			<p>周依的宽广领域以及多样的兴趣还体现在她的多媒体作品上面。例如与法国高级定制珠宝品牌Gripoix的合作当中，第一位成功获得好评的合作艺术家创作的首饰系列“菠萝密”（Pineapple’s Secret）。并且她的动画也启发了法国时尚品牌Each x Other的时装产品。</p>
+			<p>在2013年,周依也被邀请作为第一位华人设计师并且作为Iceberg的品牌创意总监完成整个Iceberg从男装,女装和配饰的设计项目。“破冰”系列的时装将在2013年12月全球</p>
+			<p>周依从2010年起重新回到中国，并在上海建立了自己的工作室及制作公司，为中国的年轻人创造一起工作及实践的机会，共同参与国际化项目。周依是一位影像的创作者及缪斯，穿梭于台前幕后，周依是当代中国版希区柯克、小野洋子及辛蒂·雪曼的混合体。目前,周依也是Tudou.com的时尚总监(中国的youtube)和Sina.com艺术和时尚顾问(中国的twitter)。</p>
+			<p>如果周依的作品不够流行,她的生命会一直拥抱和提醒她关于安迪·沃霍尔的哲学:建立她的作品成为领先的视频艺术家,她的作品分类从3D动画到雕塑、音乐作曲,电影制作,作为东方与西方文化的积极促进者，比如在关键活动TEDx(2012巴黎和2013北京)和妇女论坛。就像安迪·沃霍尔在他的时代,她的作品也被高端多样的社交圈成员认可。比如杰出的知识分子,好莱坞明星和富有的顾客和社交媒体年轻的观众。</p>
+		</div>
 		<!--intro end-->
 		<div class="links">
 			<div class="left">
-				<p>Agent:<a href="" title=""><img src="<?=get_template_directory_uri()?>/images/logo_n.jpg" alt=""></a>
+				<p>
+					Agent:
+					<a href="" title=""><img src="<?=get_template_directory_uri()?>/images/logo_n.jpg" alt=""></a>
 					<a href="" title=""><img src="<?=get_template_directory_uri()?>/images/logo_p.jpg" alt=""></a>
-					<a href="" title=""><img src="<?=get_template_directory_uri()?>/images/logo_q.jpg" alt=""></a></p>
+					<a href="" title=""><img src="<?=get_template_directory_uri()?>/images/logo_q.jpg" alt=""></a>
+				</p>
 			</div>
 			<div class="right">
 				<p>Yi Zhou is art director for :<a href=""><img src="<?=get_template_directory_uri()?>/images/logo_n_2.jpg" alt=""></a></p>
--- wp-content/themes/zhouyi/js/animate.js
+++ wp-content/themes/zhouyi/js/animate.js
@@ -14,4 +14,15 @@ jQuery(function(){
 		.mouseleave(function(){
 			jQuery(this).animate({opacity:0.5}, 300);
 		});
+		
+	jQuery('.intro-language-switcher>:button').on('click', function(){
+		
+		var targetLang = jQuery(this).attr('target-lang');
+		
+		jQuery('.intro[lang!="' + targetLang + '"]').fadeOut(300, function(){
+			
+			jQuery('.intro[lang="' + targetLang + '"]').fadeIn(300);
+		});
+		
+	});
 });
