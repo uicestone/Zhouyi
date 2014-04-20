ffd5a82b78d4141403c3e1edc6906adc2f932730
Uice Lu <uicestone@gmail.com>
2014-04-20 14:11:54 +0800

    js套一层闭包

--- wp-content/themes/zhouyi/js/animate.js
+++ wp-content/themes/zhouyi/js/animate.js
@@ -1,28 +1,30 @@
-jQuery(function(){
-	jQuery('.hoverable')
+(function($){
+$(function(){
+	$('.hoverable')
 		.mouseenter(function(){
-			jQuery(this).find('.cover').delay(500).fadeIn(300);
+			$(this).find('.cover').delay(500).fadeIn(300);
 		})
 		.mouseleave(function(){
-			jQuery(this).find('.cover').clearQueue().fadeOut(300);
+			$(this).find('.cover').clearQueue().fadeOut(300);
 		});
 	
-	jQuery('.media-print>.main>ul>li img')
+	$('.media-print>.main>ul>li img')
 		.mouseenter(function(){
-			jQuery(this).animate({opacity:1}, 300);
+			$(this).animate({opacity:1}, 300);
 		})
 		.mouseleave(function(){
-			jQuery(this).animate({opacity:0.5}, 300);
+			$(this).animate({opacity:0.5}, 300);
 		});
 		
-	jQuery('.intro-language-switcher>:button').on('click', function(){
+	$('.intro-language-switcher>:button').on('click', function(){
 		
-		var targetLang = jQuery(this).attr('target-lang');
+		var targetLang = $(this).attr('target-lang');
 		
-		jQuery('.intro[lang!="' + targetLang + '"]').fadeOut(300, function(){
+		$('.intro[lang!="' + targetLang + '"]').fadeOut(300, function(){
 			
-			jQuery('.intro[lang="' + targetLang + '"]').fadeIn(300);
+			$('.intro[lang="' + targetLang + '"]').fadeIn(300);
 		});
 		
 	});
 });
+})(jQuery);
