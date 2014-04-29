2615d5fd178816cdda5ac234d83ea30514b2690d
Uice Lu <uicestone@gmail.com>
2014-04-29 18:08:53 +0800

    single模版对AJAX请求仅显示内容

--- wp-content/themes/zhouyi/single.php
+++ wp-content/themes/zhouyi/single.php
@@ -1,1 +1,18 @@
+<?php
+if($_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest'){
+	the_post(); the_content();
+}
+else{
+	get_header();
+?>
+
+<div class="content <?=get_the_category()[0]->slug?>">
+	<?php get_sidebar(strpos(get_the_category()[0]->slug, 'print-press') !== false ? 'media' : 'artist'); ?>
+	<div class="main">
 		<?php the_post(); the_content(); ?>
+	</div>
+</div>
+<?php 
+	get_footer();
+}
+?>
