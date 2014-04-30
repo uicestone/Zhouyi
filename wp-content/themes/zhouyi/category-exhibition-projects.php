40dc7375bbf1170856e16ef2fc4ced7f916ebb64
Uice Lu <uicestone@gmail.com>
2014-04-30 09:51:32 +0800

    Resolved #36

--- wp-content/themes/zhouyi/category-exhibition-projects.php
+++ wp-content/themes/zhouyi/category-exhibition-projects.php
@@ -4,9 +4,8 @@
 	<div class="main">
 		<ul>
 			<?php while(have_posts()): the_post();?>
-			<?php $is_banner = $wp_query->current_post > 0 && $wp_query->current_post % 3 === 0; ?>
-			<li<?php if($is_banner){ ?> class="banner"<?php } ?>>
-				<a href="<?=wp_get_attachment_url(get_post_thumbnail_id(get_the_ID()))?>"><?php the_post_thumbnail($is_banner ? 'banner' : 'one-third-layout'); ?></a>
+			<li>
+				<a href="<?=wp_get_attachment_url(get_post_thumbnail_id(get_the_ID()))?>"><?php the_post_thumbnail('one-third-layout'); ?></a>
 			</li>
 			<?php endwhile; ?>
 		</ul>
