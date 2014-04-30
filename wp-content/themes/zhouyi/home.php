2f07f1e89477fc75379c5ae1f291ea0c753efd65
Uice Lu <uicestone@gmail.com>
2014-04-30 10:32:15 +0800

    Resolved #32 简介文字和首页封面图动态化
    首页大图改为intro-en这篇文章的特色图像

--- wp-content/themes/zhouyi/home.php
+++ wp-content/themes/zhouyi/home.php
@@ -1,67 +1,20 @@
-<?php get_header(); ?>
+<?php
+$intro_en = array_pop(get_posts(array('name'=>'intro-en')));
+$intro_cn = array_pop(get_posts(array('name'=>'intro-cn')));
+get_header();
+?>
 <div class="content">
-	<div class="cover"></div>
+	<div class="cover" style="background-image: url('<?=wp_get_attachment_url(get_post_thumbnail_id($intro_en->ID))?>')"></div>
 	<div class="right">
 		<div class="intro-language-switcher">
 			<button type="button" class="zh-cn" target-lang="zh-cn"></button>
 			<button type="button" class="en active" target-lang="en"></button>
 		</div>
 		<div class="intro" lang="en">
-			<p>
-				Chinese multimedia artist who has lived in Rome from the age of ten and 
-				studied between London and Paris with degrees in Political Science and 
-				Economics, Yi Zhou is a polyglot artist whose work has been shown at 
-				Shanghai Biennale, Venice Biennale and Sundance Film Festival, Cannes Film 
-				Festival since the past 10 years.
-			</p>
-			<p>
-				Yi Zhou was named as the first ever artist-ambassador to endorse a beauty 
-				brand; Clarins, from 2010 to 2011. Loved by luxury brands for her multidisciplinary range as an artist, since 2012 she has also started collaborationswith brands such as Italian luxury eyewear brand Persol and Hogan.
-			</p>
-			<p>
-				Her diverse interests and compacity to range her multimedia work have also 
-				been shown through her much-acclaimed collaborations such as with the 
-				French couture jewelry house Gripoix; a first successful artist-collection 
-				entitled “Pineapple’s Secret”. Her animations have also inspired clothing 
-				collection by the French hip brand, Each x Other.
-			</p>
-			<p>
-				In 2013, Yi Zhou has also been invited as the very first Chinese talent to 
-				be hired as a designer and creative director of a brand to do an articulated 
-				complete project ranging from men’s wear, women’s wear and accessories for 
-				Iceberg. “Breaking the Ice” capsule collection will be released in December 
-				2013 world wide.
-			</p>
-			<p>
-				Since 2010 she has relocated back to China and founded her studio and 
-				production company in Shanghai, creating job opportunities for young 
-				Chinese people to work and experiment together on international projects. 
-				An image maker and muse, switching from the front to the back of the 
-				camera, Yi Zhou is a modern-day Chinese Hitchcock, Yoko Ono and Cindy 
-				Sherman. Currently, Yi Zhou is also Tudou.com’s (Chinese would-be youtube) 
-				art-director and serves as art and fashion advisory member at Sina.com 
-				(which owns Chinese twitter).
-			</p>
-			<p>
-				If Yi Zhou’s work is not pop, the life she has been embracing reminds of Andy 
-				Warhol’s philosophy: establishing her work as leading video artist, ranging 
-				her works from 3D animations to sculptures, music composition, filmmaking, 
-				acting as active promoter of East meets West culture at key leading events 
-				such as TEDx (Paris in 2012 and Beijing in 2013) and Women’s Forum.
-			</p>
-			<p>
-				Like Andy Warhol at his time, her work is also recognized by members of 
-				highly diverse social circles such as distinguished intellectuals, Hollywood 
-				celebrities and wealthy patrons and young social media audience
-			</p>
+			<?=wpautop($intro_en->post_content)?>
 		</div>
 		<div class="intro" lang="zh-cn" style="display: none;">
-			<p>华人多媒体艺术家周依10岁定居罗马，之后在伦敦和巴黎求学并获得政治学和经济学学位。周依是一位通晓多国语言的艺术家,在过去的10年里她的作品在上海双年展，威尼斯双年展，圣丹斯电影节，以及戛纳电影节上展出。</p>
-			<p>从2010年到2011年，周依是有史以来第一位作为一个美容品牌（娇韵诗）的艺术家大使。周依作为多重身份艺术家和以她涉及多种领域的的特点使她受到奢侈品牌的喜爱。从2012年起，她开始和一些奢侈品牌进行合作，例如意大利太阳镜品牌Persol和Hogan。</p>
-			<p>周依的宽广领域以及多样的兴趣还体现在她的多媒体作品上面。例如与法国高级定制珠宝品牌Gripoix的合作当中，第一位成功获得好评的合作艺术家创作的首饰系列“菠萝密”（Pineapple’s Secret）。并且她的动画也启发了法国时尚品牌Each x Other的时装产品。</p>
-			<p>在2013年,周依也被邀请作为第一位华人设计师并且作为Iceberg的品牌创意总监完成整个Iceberg从男装,女装和配饰的设计项目。“破冰”系列的时装将在2013年12月全球</p>
-			<p>周依从2010年起重新回到中国，并在上海建立了自己的工作室及制作公司，为中国的年轻人创造一起工作及实践的机会，共同参与国际化项目。周依是一位影像的创作者及缪斯，穿梭于台前幕后，周依是当代中国版希区柯克、小野洋子及辛蒂·雪曼的混合体。目前,周依也是Tudou.com的时尚总监(中国的youtube)和Sina.com艺术和时尚顾问(中国的twitter)。</p>
-			<p>如果周依的作品不够流行,她的生命会一直拥抱和提醒她关于安迪·沃霍尔的哲学:建立她的作品成为领先的视频艺术家,她的作品分类从3D动画到雕塑、音乐作曲,电影制作,作为东方与西方文化的积极促进者，比如在关键活动TEDx(2012巴黎和2013北京)和妇女论坛。就像安迪·沃霍尔在他的时代,她的作品也被高端多样的社交圈成员认可。比如杰出的知识分子,好莱坞明星和富有的顾客和社交媒体年轻的观众。</p>
+			<?=wpautop($intro_cn->post_content)?>
 		</div>
 		<!--intro end-->
 		<div class="links">
@@ -78,11 +31,9 @@
 				<p>Art fashion advisor member for :<a href=""><img src="<?=get_template_directory_uri()?>/images/logo_n_2.jpg" alt=""></a></p>
 				<p>Tencent Beauty & Fashion ambassador :<a href=""><img src="<?=get_template_directory_uri()?>/images/logo_n_3.jpg" alt=""></a></p>
 			</div>
-
 		</div>
 
 	</div>
-	<!--intro end-->
 </div>
 
 <?php get_footer(); ?>
