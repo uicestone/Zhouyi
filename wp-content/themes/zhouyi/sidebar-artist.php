1fa8492bdb76863a7d3a2347fb725d7146640371
Uice Lu <uicestone@gmail.com>
2014-05-07 11:26:38 +0800

    Closes #39

--- wp-content/themes/zhouyi/sidebar-artist.php
+++ wp-content/themes/zhouyi/sidebar-artist.php
@@ -8,16 +8,16 @@
 	</div>
 	<ul class="artist-list">
 		<li>
-			<img src="<?= get_template_directory_uri() ?>/images/artist_iceberg.jpg">
+			<img src="<?= get_template_directory_uri() ?>/images/artist_iceberg.png">
 		</li>
 		<li>
 			<img src="<?= get_template_directory_uri() ?>/images/artist_gripoix.png">
 		</li>
+<!--		<li>
+			<img src="<?= get_template_directory_uri() ?>/images/artist_yoox.com.png">
+		</li>-->
 		<li>
-			<img src="<?= get_template_directory_uri() ?>/images/artist_yoox.com.jpg">
-		</li>
-		<li>
-			<img src="<?= get_template_directory_uri() ?>/images/artist_each-x-other.jpg">
+			<img src="<?= get_template_directory_uri() ?>/images/artist_each-x-other.png">
 		</li>
 	</ul>
 </div>
