311b13a4b3085f18115a73cd931258c534fb9a44
Uice Lu <uicestone@gmail.com>
2014-07-15 16:13:10 +0800

    艺术家系列列表不使用自动段落

--- wp-content/themes/zhouyi/category-artists.php
+++ wp-content/themes/zhouyi/category-artists.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <div class="content artist">
 	<?php get_sidebar('artist'); ?>
-	<div class="main"><?php the_post(); the_content(); ?></div>
+	<div class="main"><?php the_post(); echo $post->post_content; ?></div>
 </div>
 <?php get_footer(); ?>
