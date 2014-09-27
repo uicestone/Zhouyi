2c64b9e4c9d2f774844495da0a87df6500f67e03
Uice Lu <uicestone@gmail.com>
2014-09-27 20:41:34 +0800

    站长统计代码

--- wp-content/themes/zhouyi/footer.php
+++ wp-content/themes/zhouyi/footer.php
@@ -1,3 +1,4 @@
+		<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1253260523'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s9.cnzz.com/z_stat.php%3Fid%3D1253260523' type='text/javascript'%3E%3C/script%3E"));</script>‍
 		<?php wp_footer(); ?>
 	</body>
 </html>
