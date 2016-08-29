a50f42c30b4bdf8ece8426b8db5e73b98bff624f
Natt7 <natt.ni@qq.com>
2016-08-29 21:00:42 +0800

    更新contact

--- wp-content/themes/zhouyi/page-contact.php
+++ wp-content/themes/zhouyi/page-contact.php
@@ -2,35 +2,7 @@
 <div class="content contact">
 	<div class="map">
 		<img src="http://www.yi-yo.net/wp-content/uploads/2016/08/Social-Media-Red-Carpet.jpg">
-		<ul class="address">
-			<li>
-				YI ZHOU STUDIO
-				<br>
-				<a href="mailto:studio@yi-yo.net">
-					studio@yi-yo.net
-				</a>
-			</li>
-			<li>
-				<a href="">GALLERY REPRESENTATION:</a>
-				<br>
-				ShangHai:
-				<a href="http://www.pearllam.com" target="_blank">
-					PEAR LAM GALLERIES
-					<br>www.pearllam.com
-				</a>
-			</li>
-			<li>
-				PRESS CONTACT
-				<a href="mailto:press@yi-yo.net">Press@yi-yo.net</a>
 
-			</li>
-			<li>
-				HongKong:
-				<a href="http://www.hanart.com" target="_blank">
-					HANART TZ GALLERY
-					<br>www.hanart.com</a>
-			</li>
-		</ul>
 	</div>
 	<!--main end-->
 </div>
