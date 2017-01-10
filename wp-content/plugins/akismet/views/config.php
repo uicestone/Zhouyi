c45187cc12971c12cfca7fab275eee9532276486
Uice Lu <uicestone@gmail.com>
2017-01-10 20:34:53 +0800

    WordPress 4.7; 插件和主题更新

--- wp-content/plugins/akismet/views/config.php
+++ wp-content/plugins/akismet/views/config.php
@@ -12,28 +12,24 @@
 					<a href="<?php echo esc_url( Akismet_Admin::get_page_url( 'stats' ) ); ?>" class=""><?php esc_html_e( 'Summaries' , 'akismet');?></a>
 				</span>
 
-				<iframe allowtransparency="true" scrolling="no" frameborder="0" style="width: 100%; height: 215px; overflow: hidden;" src="<?php printf( '//akismet.com/web/1.0/snapshot.php?blog=%s&api_key=%s&height=180&locale=%s', urlencode( get_bloginfo('url') ), Akismet::get_api_key(), get_locale() );?>"></iframe>
+				<iframe allowtransparency="true" scrolling="no" frameborder="0" style="width: 100%; height: 215px; overflow: hidden;" src="<?php printf( '//akismet.com/web/1.0/snapshot.php?blog=%s&api_key=%s&height=180&locale=%s', urlencode( get_option( 'home' ) ), Akismet::get_api_key(), get_locale() );?>"></iframe>
 				<ul>
 					<li>
 						<h3><?php esc_html_e( 'Past six months' , 'akismet');?></h3>
 						<span><?php echo number_format( $stat_totals['6-months']->spam );?></span>
-						<?php esc_html_e( 'Spam blocked' , 'akismet');?>
+						<?php echo esc_html( _n( 'Spam blocked', 'Spam blocked', $stat_totals['6-months']->spam, 'akismet' ) ); ?>
 					</li>
 					<li>
 						<h3><?php esc_html_e( 'All time' , 'akismet');?></h3>
 						<span><?php echo number_format( $stat_totals['all']->spam );?></span>
-						<?php esc_html_e( 'Spam blocked' , 'akismet');?>
+						<?php echo esc_html( _n( 'Spam blocked', 'Spam blocked', $stat_totals['all']->spam, 'akismet' ) ); ?>
 					</li>
 					<li>
 						<h3><?php esc_html_e( 'Accuracy' , 'akismet');?></h3>
-						<span><?php echo $stat_totals['all']->accuracy; ?>%</span>
-						<?php printf(
-							esc_html(
-								_n( '%s missed spam, %s false positive', '%s missed spam, %s false positives', $stat_totals['all']->false_positives , 'akismet')
-							),
-							number_format( $stat_totals['all']->missed_spam ),
-							number_format( $stat_totals['all']->false_positives )
-						); ?>
+						<span><?php echo floatval( $stat_totals['all']->accuracy ); ?>%</span>
+						<?php printf( _n( '%s missed spam', '%s missed spam', $stat_totals['all']->missed_spam, 'akismet' ), number_format( $stat_totals['all']->missed_spam ) ); ?>
+						|
+						<?php printf( _n( '%s false positive', '%s false positives', $stat_totals['all']->false_positives, 'akismet' ), number_format( $stat_totals['all']->false_positives ) ); ?>
 					</li>
 				</ul>
 				<div class="clearfix"></div>
@@ -64,13 +60,43 @@
 										<tbody>
 											<?php if ( !defined( 'WPCOM_API_KEY' ) ):?>
 											<tr>
-												<th width="10%" align="left" scope="row"><?php esc_html_e('API Key', 'akismet');?></th>
+												<th class="akismet-api-key" width="10%" align="left" scope="row"><?php esc_html_e('API Key', 'akismet');?></th>
 												<td width="5%"/>
 												<td align="left">
-													<span class="api-key"><input id="key" name="key" type="text" size="15" maxlength="12" value="<?php echo esc_attr( get_option('wordpress_api_key') ); ?>" class="regular-text code <?php echo $akismet_user->status;?>"></span>
+													<span class="api-key"><input id="key" name="key" type="text" size="15" value="<?php echo esc_attr( get_option('wordpress_api_key') ); ?>" class="<?php echo esc_attr( 'regular-text code ' . $akismet_user->status ); ?>"></span>
 												</td>
 											</tr>
 											<?php endif; ?>
+											<?php if ( isset( $_GET['ssl_status'] ) ) { ?>
+												<tr>
+													<th align="left" scope="row"><?php esc_html_e( 'SSL Status', 'akismet' ); ?></th>
+													<td></td>
+													<td align="left">
+														<p>
+															<?php
+
+															if ( ! function_exists( 'wp_http_supports' ) ) {
+																?><b><?php esc_html_e( 'Disabled.', 'akismet' ); ?></b> <?php printf( esc_html( 'Your WordPress installation does not include the function %s; upgrade to the latest version of WordPress.', 'akismet' ), '<code>wp_http_supports</code>' ); ?><?php
+															}
+															else if ( ! wp_http_supports( array( 'ssl' ) ) ) {
+																?><b><?php esc_html_e( 'Disabled.', 'akismet' ); ?></b> <?php esc_html_e( 'Your Web server cannot make SSL requests; contact your Web host and ask them to add support for SSL requests.', 'akismet' ); ?><?php
+															}
+															else {
+																$ssl_disabled = get_option( 'akismet_ssl_disabled' );
+
+																if ( $ssl_disabled ) {
+																	?><b><?php esc_html_e( 'Temporarily disabled.', 'akismet' ); ?></b> <?php esc_html_e( 'Akismet encountered a problem with a previous SSL request and disabled it temporarily. It will begin using SSL for requests again shortly.', 'akismet' ); ?><?php
+																}
+																else {
+																	?><b><?php esc_html_e( 'Enabled.', 'akismet' ); ?></b> <?php esc_html_e( 'All systems functional.', 'akismet' ); ?><?php
+																}
+															}
+
+															?>
+														</p>
+													</td>
+												</tr>
+											<?php } ?>
 											<tr>
 												<th align="left" scope="row"><?php esc_html_e('Comments', 'akismet');?></th>
 												<td></td>
@@ -95,7 +121,7 @@
 													
 													printf(
 														_n(
-															'Spam in the <a href="%s">spam folder</a> older than 1 day is deleted automatically.',
+															'Spam in the <a href="%1$s">spam folder</a> older than 1 day is deleted automatically.',
 															'Spam in the <a href="%1$s">spam folder</a> older than %2$d days is deleted automatically.',
 															$delete_interval,
 															'akismet'
@@ -140,7 +166,7 @@
 											<th scope="row" align="left"><?php esc_html_e( 'Subscription Type' , 'akismet');?></th>
 											<td width="5%"/>
 											<td align="left">
-												<span><?php echo $akismet_user->account_name; ?></span>
+												<span><?php echo esc_html( $akismet_user->account_name ); ?></span>
 											</td>
 										</tr>
 										<tr>
